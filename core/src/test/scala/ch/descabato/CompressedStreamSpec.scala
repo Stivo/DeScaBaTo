9911353b8e65bb6ce8b7ddd28287fed1f1ee90ae
Stefan Ackermann <acki.ch@gmail.com>
2014-02-12 00:48:09 +0100

    Fixing test.

--- core/src/test/scala/ch/descabato/CompressedStreamSpec.scala
+++ core/src/test/scala/ch/descabato/CompressedStreamSpec.scala
@@ -52,8 +52,15 @@ class CompressedStreamSpec extends FlatSpec with BeforeAndAfterAll
     
     val encoded = baosOriginal.toByteArray(true)
     encoded(0) should be (header)
-    if (CompressionMode.lzma != compressor) // LZMA can use a small dictionary when it knows the input size in advance.
+    // LZMA and Bzip2 can set smaller dictionary size if the input size is known
+    if (CompressionMode.lzma != compressor && CompressionMode.bzip2 != compressor)
       assert(Arrays.equals(encoded.tail,compressed.toArray()))
+    else {
+      l.info(compressor+": "+encoded.tail.length+" without dict size set and "+compressed.remaining()+" with dict size set")
+      val in = new ByteArrayInputStream(Array(header)++compressed.toArray())
+      val read = CompressedStream.readStream(in).readFully
+      assert(Arrays.equals(read, toEncode))
+    }
     val in = new ByteArrayInputStream(encoded)
     val read = CompressedStream.readStream(in).readFully
     assert(Arrays.equals(read, toEncode))
