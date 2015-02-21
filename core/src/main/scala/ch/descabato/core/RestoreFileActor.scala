8c882794f19e5608cb0ae68caf2086fd571f02d9
Stefan Ackermann <acki.ch@gmail.com>
2015-02-21 05:43:31 +0100

    #2 writing asynchronously

--- core/src/main/scala/ch/descabato/core/RestoreFileActor.scala
+++ core/src/main/scala/ch/descabato/core/RestoreFileActor.scala
@@ -3,11 +3,13 @@ package ch.descabato.core
 import java.io.{File, FileOutputStream, OutputStream}
 
 import akka.actor.{PoisonPill, TypedActor}
+import akka.io.Tcp.Write
 import ch.descabato.akka.AkkaUniversePart
 import ch.descabato.utils.{CompressedStream, Utils}
 
 import scala.collection.{SortedMap, mutable}
-import scala.concurrent.{Future, Promise}
+import scala.concurrent.{Await, Future, Promise}
+import scala.concurrent.duration._
 
 trait AkkaRestoreFileHandler extends RestoreFileHandler {
   def setup(fd: FileDescription, dest: File, ownRef: AkkaRestoreFileHandler)
@@ -18,9 +20,9 @@ class RestoreFileActor extends AkkaRestoreFileHandler with Utils with AkkaUniver
   val p = Promise[Boolean]
   lazy val digest = config.getMessageDigest
 
+  var writeFileHandler: WriteFileHandler = null
   var ownRef: AkkaRestoreFileHandler = null
   var fd: FileDescription = null
-  var destination: File = null
 
   var unwrittenBlocks = SortedMap.empty[Int, Block]
   var pendingBlocks = mutable.Set.empty[Int]
@@ -32,8 +34,9 @@ class RestoreFileActor extends AkkaRestoreFileHandler with Utils with AkkaUniver
 
   def setup(fd: FileDescription, dest: File, ownRef: AkkaRestoreFileHandler): Unit = {
     this.fd = fd
-    this.destination = dest
     this.ownRef = ownRef
+    writeFileHandler = uni.actorOf[WriteFileHandler, WriteActor]("write "+dest)
+    writeFileHandler.setFile(dest)
   }
 
   override def restore(): Future[Boolean] = {
@@ -68,9 +71,7 @@ class RestoreFileActor extends AkkaRestoreFileHandler with Utils with AkkaUniver
       hashList = Array.apply(fd.hash)
     }
     numberOfBlocks = hashList.length
-    destination.getParentFile.mkdirs()
-    val fos = new FileOutputStream(destination)
-    outputStream = fos //new DigestOutputStream(fos, digest)
+
     fillUpQueue()
   }
 
@@ -84,22 +85,50 @@ class RestoreFileActor extends AkkaRestoreFileHandler with Utils with AkkaUniver
       val array = unwrittenBlocks.head._2
       unwrittenBlocks = unwrittenBlocks.tail
       blockToBeWritten += 1
-      outputStream.write(array.content)
+      writeFileHandler.write(array.content)
     }
     // if pendingblocks is smaller than maxPending and still enough to go: enqueue another one
     fillUpQueue()
     if (blockToBeWritten == numberOfBlocks) {
       // if finished:
       //    fulfill the promise
-      outputStream.close()
+      val result = Await.result(writeFileHandler.finish(), 5.minutes)
+      TypedActor.get(uni.system).getActorRefFor(writeFileHandler) ! PoisonPill
 //      if (!util.Arrays.equals(fd.hash, digest.digest())) {
 //        l.warn("Could not reconstruct file " + destination.getAbsolutePath)
 //        p.success(false)
 //      } else {
-        p.success(true)
+        p.success(result)
 //      }
       TypedActor.get(uni.system).getActorRefFor(ownRef) ! PoisonPill
     }
   }
 
+}
+
+trait WriteFileHandler {
+  def setFile(file: File)
+  def write(bytes: Array[Byte])
+  def finish(): Future[Boolean]
+}
+
+class WriteActor extends WriteFileHandler with AkkaUniversePart {
+  var fos: FileOutputStream = null
+  override def setFile(destination: File): Unit = {
+    destination.getParentFile.mkdirs()
+    fos = new FileOutputStream(destination)
+  }
+
+  override def finish(): Future[Boolean] = {
+    try {
+      fos.close()
+      Future.successful(true)
+    } catch {
+      case e => Future.failed(e)
+    }
+  }
+
+  override def write(bytes: Array[Byte]): Unit = {
+    fos.write(bytes)
+  }
 }
