49c08eea2a0abb46a58af77590c9afd72891a5d3
Stivo <stivo@gmx.ch>
2018-01-10 22:24:23 +0100

    Fix compile error

--- integrationtest/src/test/scala/ch/descabato/it/tests/SmartCompressionTest.scala
+++ integrationtest/src/test/scala/ch/descabato/it/tests/SmartCompressionTest.scala
@@ -2,7 +2,7 @@ package ch.descabato.it.tests
 
 import ch.descabato.it.IntegrationTest
 
-public class SmartCompressionTest extends IntegrationTest {
+class SmartCompressionTest extends IntegrationTest {
 
     testWith("backup with smart compression", " --compression smart --threads 8 --volume-size 20Mb", "", 1, "200Mb", crash = false)
 
