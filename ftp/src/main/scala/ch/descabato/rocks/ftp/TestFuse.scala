c3479fc1df182488137d38d37d1c9ae7da7c46ee
Stefan Ackermann <stivo@gmx.ch>
2020-10-10 09:46:18 +0200

    Improvements for mounting in fuse

--- ftp/src/main/scala/ch/descabato/rocks/ftp/TestFuse.scala
+++ ftp/src/main/scala/ch/descabato/rocks/ftp/TestFuse.scala
@@ -1,11 +1,12 @@
 package ch.descabato.rocks.ftp
 
 import java.io.File
+import java.io.InputStream
 import java.nio.file.Paths
-import java.util.Objects
 
 import ch.descabato.core.config.BackupFolderConfiguration
 import ch.descabato.rocks.RocksEnv
+import ch.descabato.utils.Utils
 import jnr.ffi.Platform
 import jnr.ffi.Platform.OS
 import jnr.ffi.Pointer
@@ -37,6 +38,7 @@ object TestFuse {
       stub.mount(Paths.get(path), true, false)
     } finally stub.umount()
   }
+
 }
 
 class TestFuse(reader: BackupReader) extends FuseStubFS {
@@ -67,10 +69,29 @@ class TestFuse(reader: BackupReader) extends FuseStubFS {
         stat.st_nlink.set(1)
         stat.st_size.set(x.metadata.length)
         stat.st_mtim.tv_sec.set(x.fileMetadataKeyWrapper.fileMetadataKey.changed / 1000)
+        val createdTime = if (path.startsWith("/" + PathUtils.allFilesPath)) {
+          val hour = 1000L * 60 * 60
+          12 * hour + (x.linkCount - 1) * 24 * hour
+        } else {
+          x.metadata.created / 1000
+        }
+        stat.st_ctim.tv_sec.set(createdTime)
+        val totalSize = reader.rocksEnv.rocks.getIndexes(x.metadata).map(_.lengthCompressed.toLong).sum
+        println(s"Total size for $path is ${Utils.readableFileSize(x.metadata.length)}, but with compression: ${Utils.readableFileSize(totalSize)}. ")
       case None =>
         res = -ErrorCodes.ENOENT()
     }
     res
+  }
+
+  override def getxattr(path: String, name: String, value: Pointer, size: Long): Int = {
+    println(s"Getting extended attribute $name for $path with size $size")
+    super.getxattr(path, name, value, size)
+  }
+
+  override def listxattr(path: String, list: Pointer, size: Long): Int = {
+    println(s"Listing extended attributes for $path")
+    super.listxattr(path, list, size)
   }
 
   override def readdir(path: String, buf: Pointer, filter: FuseFillDir, @off_t offset: Long, fi: FuseFileInfo): Int = {
@@ -96,16 +117,36 @@ class TestFuse(reader: BackupReader) extends FuseStubFS {
     }
   }
 
+  var cache: Map[CacheEntry, InputStream] = Map.empty
+
+  case class CacheEntry(path: String, offset: Long)
+
   override def read(path: String, buf: Pointer, @size_t sizeIn: Long, @off_t offset: Long, fi: FuseFileInfo): Int = {
     lookup(path) match {
       case Some(x: FileNode) =>
-        val input = reader.createInputStream(x.metadata)
-        input.skip(offset)
-        val bytes = Array.ofDim[Byte](sizeIn.toInt)
-        val read = IOUtils.readFully(input, bytes)
-        buf.put(0, bytes, 0, read)
-        println(s"Read from $path at $offset with length $sizeIn, returned $read bytes")
-        read
+        this.synchronized {
+          val entry = CacheEntry(path, offset)
+          val input = cache.get(entry) match {
+            case Some(x) =>
+              cache -= entry
+              println(s"Cache hit on $entry")
+              x
+            case None =>
+              val input = reader.createInputStream(x.metadata)
+              input.skip(offset)
+              input
+          }
+          val bytes = Array.ofDim[Byte](sizeIn.toInt)
+          val read = IOUtils.readFully(input, bytes)
+          buf.put(0, bytes, 0, read)
+          println(s"Read from $path at $offset with length $sizeIn, returned $read bytes")
+          if (offset + read < x.metadata.length) {
+            val entry1 = entry.copy(offset = offset + read)
+            println(s"Added cache entry for $entry1")
+            cache += entry1 -> input
+          }
+          read
+        }
       case _ =>
         -ErrorCodes.ENOENT()
     }
