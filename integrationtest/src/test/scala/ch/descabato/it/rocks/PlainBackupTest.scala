b936c5baea4013056ad9bf77b9f8de05058d1d27
Stefan Ackermann <stivo@gmx.ch>
2021-11-11 22:30:30 +0100

    Rename test

--- integrationtest/src/test/scala/ch/descabato/it/rocks/PlainBackupTest.scala
+++ integrationtest/src/test/scala/ch/descabato/it/rocks/PlainBackupTest.scala
@@ -2,7 +2,7 @@ package ch.descabato.it.rocks
 
 import ch.descabato.it.IntegrationTest
 
-class PlainRocksBackupTest extends IntegrationTest {
+class PlainBackupTest extends IntegrationTest {
 
     testWith("plain backup", " --compression gzip", "", 1, "10Mb")
 
